17b9cad3d3bba3a1458d91cc58e630c1a8ab674b
Kerry McCullough <subone@subone.org>
2018-06-18 12:56:50 -0600

    Simple unsorted leaderboard output

--- leaderboard.js
+++ leaderboard.js
@@ -1,4 +1,4 @@
-(function(window, undefined) {
+(function(window, document, undefined) {
 
   var leaderboard = [];
 
@@ -13,6 +13,8 @@
   }
 
   function getRandomData() {
+    var minWinnings = Math.pow(10, 5);
+    var maxWinnings = Math.pow(10, 8);
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
       if (request.readyState !== XMLHttpRequest.DONE || request.status !== 200) {
@@ -27,40 +29,97 @@
           name: capitalize(result.name.first) + ' ' + capitalize(result.name.last),
           nativeOf: result.nat,
           icon: result.picture.thumbnail,
-          winnings: result.phone
+          winnings: Math.floor(Math.random() * (maxWinnings - minWinnings) + minWinnings)
         };
       });
-      console.log(leaderboard);
       // Update the view with the new data
       updateLeaderboard();
     };
-    request.open('GET', 'https://randomuser.me/api/?inc=name,picture,phone,nat&results=10', true);
+    request.open('GET', 'https://randomuser.me/api/?inc=name,picture,nat&results=10', true);
     request.send();
   }
 
-  function updateLeaderboard() {
-    // TODO: sort leaderboard, extract median and mean
-
-    // var t = document.querySelector('#tpl-leaderboard');
-    //
-    // // Clone the new row and insert it into the table
-    // var tb = document.querySelector("tbody");
-    // var clone = document.importNode(t.content, true);
-    // td = clone.querySelectorAll("td");
-    // td[0].textContent = "1235646565";
-    // td[1].textContent = "Stuff";
-    //
-    // tb.appendChild(clone);
-    //
-    // // Clone the new row and insert it into the table
-    // var clone2 = document.importNode(t.content, true);
-    // td = clone2.querySelectorAll("td");
-    // td[0].textContent = "0384928528";
-    // td[1].textContent = "Acme Kidney Beans 2";
-    //
-    // // <img src="http://www.countryflags.io/:country_code/:style/:size.png">
-    //
-    // tb.appendChild(clone2);
+  function clearElementChildren(el) {
+    while (el.firstChild) {
+      el.removeChild(el.firstChild);
+    }
+    return el;
   }
 
-})(window);
+  function dollarFilter(value) {
+    value = Math.floor(value);
+    var v = '' + value;
+    if (value >= Math.pow(10, 5)) {
+      value = (v.length > 6 ? v.substring(0, v.length - 6) : '0')
+        + '.' + v.substr(v.length - 6, 1) + 'M';
+    } else if (value >= Math.pow(10, 3)) {
+      value = v.substring(0, v.length - 3)
+        + '.' + v.substr(v.length - 3, 1) + 'K';
+    }
+    return '$' + value;
+  }
+
+  function getLeaderboardEntryNode(entry) {
+    var entryTpl = document.querySelector('#tpl-leaderboard__entry');
+    var entryEl = document.importNode(entryTpl.content, true);
+    var fieldEl;
+
+    // Leaderboard Index
+    fieldEl = entryEl.querySelector('.js-leaderboard-entry__index');
+    fieldEl.textContent = entry.index + '.';
+
+    // Player Icon
+    fieldEl = entryEl.querySelector('.js-leaderboard-entry__player-icon');
+    fieldEl.src = '' + entry.icon;
+
+    // Player Name
+    fieldEl = entryEl.querySelector('.js-leaderboard-entry__player-name');
+    fieldEl.textContent = '' + entry.name;
+
+    // Player Winnings
+    fieldEl = entryEl.querySelector('.js-leaderboard-entry__winnings');
+    fieldEl.textContent = dollarFilter(entry.winnings);
+
+    // Player Nationality Flag
+    fieldEl = entryEl.querySelector('.js-leaderboard-entry__flag');
+    fieldEl.src = 'http://www.countryflags.io/' + entry.nativeOf + '/flat/48.png';
+
+    // Player Nationality
+    fieldEl = entryEl.querySelector('.js-leaderboard-entry__country');
+    fieldEl.textContent = entry.nativeOf;
+
+    return entryEl;
+  }
+
+  function updateLeaderboard() {
+    var leaderboardTpl, leaderboardEl;
+    var el = document.querySelector('.js-leaderboard');
+    clearElementChildren(el);
+
+    // No Tournament Results
+    if (!leaderboard.length) {
+
+      leaderboardTpl = document.querySelector('#tpl-leaderboard--empty');
+      leaderboardEl = document.importNode(leaderboardTpl.content, true);
+
+    // Tournament Results
+    } else {
+
+      leaderboardTpl = document.querySelector('#tpl-leaderboard');
+      leaderboardEl = document.importNode(leaderboardTpl.content, true);
+
+      var entriesEl = leaderboardEl.querySelector('.js-leaderboard__entries');
+
+      leaderboard
+        // TODO: sort leaderboard, extract median and mean
+        .forEach(function (entry, index) {
+          entry.index = index + 1;
+          entriesEl.appendChild(getLeaderboardEntryNode(entry));
+        });
+
+    } // Tournament Results
+
+    el.appendChild(leaderboardEl);
+  }
+
+})(window, document);
